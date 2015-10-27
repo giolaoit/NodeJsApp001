8b801e747f5f91bc489f4fa126861b6d5935da2c
Tuan Hai <tuanhaicntt@gmail.com>
2015-10-27 14:00:06 +0700

    2

--- app.js
+++ app.js
@@ -1,13 +1,40 @@
-var routes = require('./routes'),
-	express = require('express');
+/*eslint-env node*/
 
+//------------------------------------------------------------------------------
+// node.js starter application for Bluemix
+//------------------------------------------------------------------------------
+
+// This application uses express as its web server
+// for more info, see: http://expressjs.com
+var express = require('express');
+
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
+
+// create a new express server
 var app = express();
 
-app.get('/', function(req, res){
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+//Route
+var routes = require('./routes');
+
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+
+	// print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
+
+app.get('/api/v1/about', function(req, res){
 	res.type('text/plain');
 	res.send('Iam a beautiful butterfly');
 });
 
 app.post('/api/v1/register', routes.register);
 
-app.listen(8888);
